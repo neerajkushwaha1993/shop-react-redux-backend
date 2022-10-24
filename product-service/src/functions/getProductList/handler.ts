f494d4ffc9666e053f6902818a615e5fabcb7de1
Neeraj Kushwaha <neeraj_kushwaha@epam.com>
2022-10-24 14:13:34 +0530

    integration with dynamoDB

--- product-service/src/functions/getProductList/handler.ts
+++ product-service/src/functions/getProductList/handler.ts
@@ -1,13 +1,57 @@
 import { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
-import { products } from '../../mocks/data';
 import schema from './schema';
+import { db,ProductsTableName,StocksTableName } from 'src/utils/dynamoDB';
+import { DEFAULT_HEADERS } from 'src/utils/common';
+
+
+const getProducts = async () => {
+  return await db
+      .scan({
+          TableName: ProductsTableName,
+      })
+      .promise();
+};
+
+
+const getStocks = async () => {
+  return await db
+      .scan({
+          TableName: StocksTableName,
+      })
+      .promise();
+};
+
+const mergeResults = (productItems, stockItems) => {
+  const productsMap = new Map();
+
+  productItems.forEach((product) => {
+      const id = product.id;
+      productsMap.set(id, product);
+  });
+
+  stockItems.forEach((stock) => {
+      const id = stock.product_id;
+      const item = productsMap.get(id);
+      if (item) {
+          item.count = stock.count;
+          productsMap.set(id, item);
+      }
+  });
+
+  return Array.from(productsMap.values());
+};
 
 const getProductList:ValidatedEventAPIGatewayProxyEvent<
 typeof schema> = async () => {
+
+  const [products, stocks] = await Promise.all([getProducts(), getStocks()]);
+  const items = mergeResults(products.Items, stocks.Items);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(products)
+    headers: DEFAULT_HEADERS,
+    body: JSON.stringify(items)
   }
 };
 
